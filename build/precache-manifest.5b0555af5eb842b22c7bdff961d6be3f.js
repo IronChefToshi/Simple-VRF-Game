6c0454e1a3f1230d2705ad306442a194bca400a7
ironcheftoshi <ironcheftoshi@gmail.com>
2021-01-15 00:58:55 -0600

    message

---
+++ build/precache-manifest.5b0555af5eb842b22c7bdff961d6be3f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087769024da98116b57f8a002589effa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa41f707085fc3cdb7fb",
+    "url": "./static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "4bd4ca592bf3a4373ea9",
+    "url": "./static/css/main.0def4619.chunk.css"
+  },
+  {
+    "revision": "fa41f707085fc3cdb7fb",
+    "url": "./static/js/2.55fcdf8e.chunk.js"
+  },
+  {
+    "revision": "3076bc301cd9ac8a6e5baf44c2b789b6",
+    "url": "./static/js/2.55fcdf8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd4ca592bf3a4373ea9",
+    "url": "./static/js/main.d27c8dad.chunk.js"
+  },
+  {
+    "revision": "cd65410e499caeea6ae1",
+    "url": "./static/js/runtime-main.fe88992c.js"
+  },
+  {
+    "revision": "fc8d2280a067dd89ddd7af8dc1662eba",
+    "url": "./static/media/dice.fc8d2280.webp"
+  },
+  {
+    "revision": "53f49b581bd0dea8893033d43f28bd42",
+    "url": "./static/media/dice_logo.53f49b58.png"
+  },
+  {
+    "revision": "c0ab6380bf570f8c0ce0e632adb0a2c7",
+    "url": "./static/media/dice_rolling.c0ab6380.gif"
+  },
+  {
+    "revision": "cd488de327fe726ee152d0f43dce93ff",
+    "url": "./static/media/eth.cd488de3.png"
+  }
+]);
